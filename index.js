d138b2eb48fb3e40dec7282b40d88e7ca5d16d06
Vidhya0501 <vidhyasuri97@gmail.com>
2023-10-26 13:03:30 +0530

    cors updated

--- index.js
+++ index.js
@@ -9,13 +9,26 @@ const dotenv=require('dotenv')
 const UserAccountModel=require('./models/UserAccountM')
 
 const app=express()
-app.use(cors(
-    {
-        origin:["http://localhost:5173"],
-        methods:["POST","GET"],
-        credentials:true
+// app.use(cors(
+//     {
+//         origin:["http://localhost:5173"],
+//         methods:["POST","GET"],
+//         credentials:true
+//     }
+// ))
+
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = ['http://localhost:3001', 'https://reset-password-49fq.onrender.com'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
     }
-))
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+  });
 app.use(express.json())
 app.use(cookieParser())
 dotenv.config()
